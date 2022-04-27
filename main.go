432a6c78c5431e22f2e17ab04036fc1b3b674b01
Vasyl Haievyi <vasha375@student.liu.se>
2022-04-27 08:26:03 +0200

    Server. Added categories endpoint.

--- main.go
+++ main.go
@@ -5,5 +5,25 @@ import (
 )
 
 func main() {
+	router := gin.Default()
 
+	api := router.Group("/api")
+	{
+		api.GET("/categories", getCategoriesHandler)
+	}
+
+	router.Run(":8080")
+}
+
+func getCategoriesHandler(ctx *gin.Context) {
+	categories := [...]gin.H{
+		{"id": "transport", "prettyName": "Transport"},
+		{"id": "real_estate", "prettyName": "Real Estate"},
+		{"id": "electronics", "prettyName": "Electronics"},
+		{"id": "fascion", "prettyName": "Fascion"},
+		{"id": "sport_and_hobbies", "prettyName": "Sport & Hobbies"},
+	}
+	ctx.JSON(200, gin.H{
+		"categories": categories,
+	})
 }
