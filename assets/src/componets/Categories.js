6fee8e8ab897efc3c9b2defbe0bc447ed0a06495
Vasyl Haievyi <vasha375@student.liu.se>
2022-04-27 08:26:44 +0200

    Client. Added usage of categories endpoint.

--- assets/src/componets/Categories.js
+++ assets/src/componets/Categories.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom'
 
 import styles from './Categories.module.css'
 
-class Categories extends React.Component {
-    render() { 
-        let categories = this.props.categories.map((category) => {
-            return (
-                <div className={styles.category}>
-                    <div className={styles.categoryImage}></div>
-                    <div className={styles.categoryName}>
-                        <Link to={category.id}>{category.prettyName}</Link>  
-                    </div>
-                </div>
-            )
-        });
+function Categories()  {
+    let [categories, setCategories] = useState([])
+    useEffect(() => {
+        fetch("/api/categories")
+        .then(response => response.json()
+        .then(data => setCategories(data.categories)))
+    }, [])
+
+    let categoriesItems = categories.map((category) => {
         return (
-            <div className={styles.categoriesContainer}>
-                {categories}
+            <div className={styles.category}>
+                <div className={styles.categoryImage}></div>
+                <div className={styles.categoryName}>
+                    <Link to={`/search?category=${category.id}`}>{category.prettyName}</Link>  
+                </div>
             </div>
         )
-    }
+    });
+    return (
+        <div className={styles.categoriesContainer}>
+            {categoriesItems}
+        </div>
+    )
 }
 
 export default Categories;
+
+function getCategories() {
+    return [{id: "transport", prettyName: "Transport"}, {id: "real_estate", prettyName: "Real Estate"}, {id: "electronics", prettyName: "Electronics"}, {id: "fascion", prettyName: "Fascion" }, {id: "sport_and_hobbies", prettyName: "Sport & Hobbies"},]
+  }
--- assets/src/componets/Main.js
+++ assets/src/componets/Main.js
@@ -4,16 +4,12 @@ import NavBar from './NavBar';
 import SearchBar from './SearchBar';
 import Categories from './Categories';
 
-function getCategories() {
-  return [{id: "transport", prettyName: "Transport"}, {id: "real_estate", prettyName: "Real Estate"}, {id: "electronics", prettyName: "Electronics"}, {id: "fascion", prettyName: "Fascion" }, {id: "sport_and_hobbies", prettyName: "Sport & Hobbies"},]
-}
-
 function Main() {
   return (
     <div className={styles.main}>
       <NavBar />
       <SearchBar />
-      <Categories categories={getCategories()} />
+      <Categories />
     </div>
   );
 }
