47a702f1b164ec608e2f3dc4114746be609aad70
Vasyl Haievyi <vasha375@student.liu.se>
2022-06-05 21:00:09 +0200

    Client. Replaced search mockup

--- assets/src/componets/Search.js
+++ assets/src/componets/Search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSearchParams } from 'react-router-dom'
 import { Container, Row, Col } from 'react-bootstrap'
 
@@ -6,9 +6,12 @@ import NavBar from './NavBar'
 import SearchBar from './SearchBar'
 import SearchResults from './SearchResults';
 import SearchOptions from './SearchOptions';
+import axios from 'axios';
 
 function Search () {
     let [searchParams, setSearchParams] = useSearchParams();
+    let [searchResults, setSearchResults] = useState([]);
+    let [categoriesFilter, setCategoriesFilter] = useState([])
 
     let onCategoryChecked = (id ,checked) => {
         if (checked) {
@@ -20,12 +23,21 @@ function Search () {
         }
 
         searchParams.sort()
+        setCategoriesFilter(searchParams.getAll('category'))
         setSearchParams(searchParams)
     }
 
     let onSearchClicked = (searchTerm) => {
         searchParams.set('q', searchTerm)
         setSearchParams(searchParams)
+        
+        let searchURL = '/api/items?q=' + searchTerm + '&' + categoriesFilter.map(c => 'category=' + c).join('&')
+        axios.get(searchURL)
+        .then(response => {
+            setSearchResults(response.data.items)
+        }).catch(error => {
+            alert(JSON.stringify(error))
+        })
     }
 
     return (
@@ -41,23 +53,11 @@ function Search () {
                     <SearchOptions checked={searchParams.getAll('category')} onCheckedChange={onCategoryChecked} />
                 </Col>
                 <Col>
-                    <SearchResults results={getSearchResults()} />
+                    <SearchResults results={searchResults} />
                 </Col>
             </Row>
         </Container>
     )
-}
-
-function getSearchResults() {
-    let res = [];
-    for (var i = 0; i < 30; i++) {
-        res.push({
-            id: i.toString(),
-            title: "Result " + i.toString() + (i % 5 === 0? " blablblalbalbalbalblablablabla" : ""),
-        })
-    }
-
-    return res;
 }
 
 export default Search;
