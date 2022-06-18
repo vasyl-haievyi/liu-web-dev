1808959c4d5943456f34985e712bd8c0947f2ec5
Vasyl Haievyi <vasha375@student.liu.se>
2022-06-18 22:45:14 +0200

    Log index.html error

--- internal/server/router.go
+++ internal/server/router.go
@@ -47,13 +47,14 @@ func setRouter() *chi.Mux {
 	})
 
 	router.Get("/static/*", func(w http.ResponseWriter, r *http.Request) {
-		dir := http.Dir("../assets/build/static")
+		dir := http.Dir("./assets/build/static")
 
 		http.StripPrefix("/static/", http.FileServer(dir)).ServeHTTP(w, r)
 	})
 
-	index, _ := ioutil.ReadFile("../assets/build/index.html")
+	index, err := ioutil.ReadFile("./assets/build/index.html")
 	fmt.Println(os.Getwd())
+	fmt.Println(err)
 	router.Get("/*", func(w http.ResponseWriter, r *http.Request) {
 		w.Write(index)
 	})
